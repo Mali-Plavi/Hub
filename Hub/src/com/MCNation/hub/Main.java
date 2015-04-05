e7211215aef5dec188f9b5454ec61d4ef5394929
moonshyne1s <allen.n.siler@gmail.com>
2015-04-05 14:51:39 -0400

    Made warps.

--- Hub/src/com/MCNation/hub/Main.java
+++ Hub/src/com/MCNation/hub/Main.java
@@ -9,7 +9,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 
-		@Override
+		public final Location[] warpLocations = new Location[100];
+		public final String[] warpNames = new String[100];
+		public int warpCounter = 0;
+	    
+	    @Override
 		public void onEnable() {
 			this.getServer().getPluginManager().registerEvents(new Listener(), this);
 			this.getConfig().options().copyDefaults(true);
@@ -37,6 +41,41 @@ public class Main extends JavaPlugin{
 					Player targetPlayer1 = player.getServer().getPlayer(args[1]);
 				    Location targetPlayer1Location = targetPlayer1.getLocation();
 				    targetPlayer.teleport(targetPlayer1Location);
+				}
+			}
+			if(commandLabel.equalsIgnoreCase("setwarp")) {
+				if(args.length == 0) {
+					player.sendMessage(ChatColor.RED + "/setwarp <warpname>");
+				}else {
+					Location location = player.getLocation();
+					if(!(warpCounter > 100)) {
+						warpLocations[warpCounter] = location;
+						warpNames[warpCounter] = args[0];
+						warpCounter++;
+						player.sendMessage(ChatColor.GREEN + "Warp Set As: " + args[0]);
+					}else {
+						player.sendMessage(ChatColor.RED + "Warp Limit Exceeded! Unabled To Create Warp!");
+						
+					}
+				}
+			}else if(commandLabel.equalsIgnoreCase("warp")) {
+				for(int i = 0; i < warpNames.length; i++) {
+					String warpName = warpNames[i];
+					if(args[0].equalsIgnoreCase(warpName)) {
+						Location warpLocation = warpLocations[i];
+						player.teleport(warpLocation);
+						player.sendMessage(ChatColor.GREEN + "Teleported to " + warpName);
+						break;
+					}
+				}
+			}else if(commandLabel.equalsIgnoreCase("warps")) {
+				String warps = "";
+				for(int i = 0; i <warpNames.length; i++) {
+					if(i !=warpNames.length) {
+						warps+= warpNames[i] + ", ".replace("null", "");
+					}else {
+						player.sendMessage(ChatColor.DARK_GRAY + "Showing All Warps: " + ChatColor.GRAY + warps);
+					}
 				}
 			}
 			return false;
