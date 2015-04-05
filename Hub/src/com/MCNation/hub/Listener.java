960c38f06a39b7d83df96004a6c5742147c613a8
Mali-Plavi <marko.raspica@gmail.com>
2015-04-05 18:15:19 +0200

    Added inv shit

--- Hub/src/com/MCNation/hub/Listener.java
+++ Hub/src/com/MCNation/hub/Listener.java
@@ -1,16 +1,23 @@
 package com.MCNation.hub;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class Listener implements org.bukkit.event.Listener{
 	//Gadgets
+		//Cosmetic
+	public Inventory cosmetic;
+	
 	@EventHandler
 	public void playerJoinEvent(PlayerJoinEvent e){
 		Player p = e.getPlayer();
@@ -21,6 +28,17 @@ public class Listener implements org.bukkit.event.Listener{
 		i.setItemMeta(m);
 		
 		p.getInventory().setItem(4, i);
+	}
+	
+	@EventHandler
+	public void PlayerInteract(PlayerInteractEvent e){
+		Action a = e.getAction();
+		if(a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK){
+			cosmetic = Bukkit.createInventory(null, 45);
+			if(e.getPlayer().getItemInHand().getItemMeta().getDisplayName().contains("Cosmetic Menu")){
+				e.getPlayer().openInventory(cosmetic);
+			}
+		}
 	}
 	
 	
