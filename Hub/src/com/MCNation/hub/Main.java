720f5d9614673bd66110a00d974621f86e5a9c70
moonshyne1s <allen.n.siler@gmail.com>
2015-04-05 12:40:37 -0400

    Added /help.

--- Hub/src/com/MCNation/hub/Main.java
+++ Hub/src/com/MCNation/hub/Main.java
@@ -1,5 +1,9 @@
 package com.MCNation.hub;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -13,5 +17,13 @@ public class Main extends JavaPlugin{
 		@Override
 		public void onDisable() {
 			
+		}
+		
+		public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+			Player player = (Player) sender;
+			if(commandLabel.equalsIgnoreCase("help")) {
+				player.sendMessage(ChatColor.GOLD + "A more comprehensive help system will come in the future.");
+			}
+			return false;
 		}
 }
